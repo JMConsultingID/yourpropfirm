cf0492f83ef308bf903003c82303ea10b3906a75
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-24 19:39:24 +0800

    Update yourpropfirm-public-functions-order.php

--- inc/public/yourpropfirm-public-functions-order.php
+++ inc/public/yourpropfirm-public-functions-order.php
@@ -7,29 +7,33 @@
  *
  * @package yourpropfirm
  */
+
 function yourpropfirm_program_id_post_meta_on_order_creation($order_id) {
-    global $woocommerce;
-    $items = wc_get_order($order_id)->get_items();
-    foreach ($items as $item) {
+    $order = wc_get_order($order_id);
+    if (!$order) {
+        return;
+    }
+
+    foreach ($order->get_items() as $item_id => $item) {
         $product_id = $item->get_product_id();
-        $program_id = get_post_meta($product_id, '_program_id', true);
-        // Update order meta with program ID name
-        update_post_meta($order_id, 'yourpropfirm_program_id', $program_id);
+        if ($product_id) {
+            $program_id = get_post_meta($product_id, '_yourpropfirm_program_id', true);
+            if (!empty($program_id)) {
+                update_post_meta($order_id, 'yourpropfirm_program_id', $program_id);
+            }
+        }
     }
 }
-add_action('woocommerce_new_order', 'yourpropfirm_program_id_post_meta_on_order_creation');
 
 function yourpropfirm_mt_version_post_meta_on_order_creation($order_id) {
     $default_mt = get_option('yourpropfirm_connection_default_mt_version_field');
     $mt_version = isset($_POST['yourpropfirm_mt_version']) ? $_POST['yourpropfirm_mt_version'] : '';
-    if (!empty($mt_version)){
+    if (!empty($mt_version)) {
         $mt_version_value = $mt_version;
-    }
-    else{
-        if (!empty($default_mt)){
+    } else {
+        if (!empty($default_mt)) {
             $mt_version_value = $default_mt;
-        }
-        else{
+        } else {
             $mt_version_value = 'CTrader';
         }
     }
@@ -43,19 +47,18 @@ function yourpropfirm_additional_post_meta_on_order_creation($order_id) {
 
 function yourpropfirm_display_order_meta_after_billing_admin_order($order) {
     $order_id = $order->get_id();
-    $yourpropfirm_programId = get_post_meta($order_id, 'yourpropfirm_program_id', true);
+    $yourpropfirm_program_id = get_post_meta($order_id, 'yourpropfirm_program_id', true);
     $yourpropfirm_meta_version = get_post_meta($order_id, 'yourpropfirm_mt_version', true);
     $yourpropfirm_connection_completed = get_post_meta($order_id, 'yourpropfirm_connection_completed', true);
-    echo '<h3>' . __('YourPropFirm Program Details') . '</h3>';
-    
-    echo '<p><strong>' . __('YourPropFirm programID') . ':</strong> ' . esc_html($yourpropfirm_programId) . '</p>';
-    echo '<p><strong>' . __('YourPropFirm MetaVersion') . ':</strong> ' . esc_html($yourpropfirm_meta_version) . '</p>';
-    echo '<p><strong>' . __('YourPropFirm Completed') . ':</strong> ' . esc_html($yourpropfirm_connection_completed) . '</p>';
-    echo '</div>';
 
+    echo '<h3>' . __('YourPropFirm Program Details') . '</h3>';
+    echo '<p><strong>' . __('YourPropFirm Program ID') . ':</strong> ' . esc_html($yourpropfirm_program_id) . '</p>';
+    echo '<p><strong>' . __('YourPropFirm Meta Version') . ':</strong> ' . esc_html($yourpropfirm_meta_version) . '</p>';
+    echo '<p><strong>' . __('YourPropFirm Completed') . ':</strong> ' . esc_html($yourpropfirm_connection_completed) . '</p>';
 }
 
-
+add_action('woocommerce_new_order', 'yourpropfirm_program_id_post_meta_on_order_creation');
 add_action('woocommerce_new_order', 'yourpropfirm_mt_version_post_meta_on_order_creation');
 add_action('woocommerce_new_order', 'yourpropfirm_additional_post_meta_on_order_creation');
 add_action('woocommerce_admin_order_data_after_billing_address', 'yourpropfirm_display_order_meta_after_billing_admin_order', 10, 1);
+?>
