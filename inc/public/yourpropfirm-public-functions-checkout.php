45ed0cfeddac2a902590194f58078d3a38945280
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-24 14:32:03 +0800

    Update yourpropfirm-public-functions-checkout.php

--- inc/public/yourpropfirm-public-functions-checkout.php
+++ inc/public/yourpropfirm-public-functions-checkout.php
@@ -20,46 +20,46 @@ function yourpropfirm_display_custom_field_after_billing_form() {
 
     // Initialize options array
     // Initialize options array with a default 'select' option
-    $options = ['' => __('Select Meta Trader Version', ' yourpropfirm')]; // Default prompt option
+    $options = ['' => __('Select Meta Trader Version', 'yourpropfirm')]; // Default prompt option
 
 
     // Determine options based on enable_mt_ctrader setting
     if ($enable_mt_ctrader === 'enable') {
-        $options['CTrader'] = __('CTrader', ' yourpropfirm');
+        $options['CTrader'] = __('CTrader', 'yourpropfirm');
     } else {
         if ($default_mt === 'CTrader') {
             $options = array(
-                '' => __('Select MetaTrader Version*', ' yourpropfirm'),
-                'MT4' => __('MetaTrader Version 4', ' yourpropfirm'),
-                'MT5' => __('MetaTrader Version 5', ' yourpropfirm'),
+                '' => __('Select MetaTrader Version*', 'yourpropfirm'),
+                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
+                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
             );
         } elseif ($default_mt === 'MT5') {
             $options = array(
-                '' => __('Select MetaTrader Version*', ' yourpropfirm'),
-                'MT5' => __('MetaTrader Version 5', ' yourpropfirm'),
-                'MT4' => __('MetaTrader Version 4', ' yourpropfirm'),
+                '' => __('Select MetaTrader Version*', 'yourpropfirm'),
+                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
+                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
             );
         } elseif ($default_mt === 'MT4') {
             $options = array(
-                '' => __('Select MetaTrader Version*', ' yourpropfirm'),
-                'MT4' => __('MetaTrader Version 4', ' yourpropfirm'),
-                'MT5' => __('MetaTrader Version 5', ' yourpropfirm'),
+                '' => __('Select MetaTrader Version*', 'yourpropfirm'),
+                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
+                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
             );
         } else { // Default to MT4 if not MT5 or CTrader
             $options = array(
-                '' => __('Select MetaTrader Version*', ' yourpropfirm'),         
-                'MT4' => __('MetaTrader Version 4', ' yourpropfirm'),
-                'MT5' => __('MetaTrader Version 5', ' yourpropfirm'),
+                '' => __('Select MetaTrader Version*', 'yourpropfirm'),         
+                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
+                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
             );
         }
     }
 
 
-    if ($checkout_form === ' yourpropfirm_form') {
+    if ($checkout_form === 'woocommerce_form') {
         ?>
         <div class="yourpropfirm_mt_version yourpropfirm_mt_version_field_wrapper">
             <?php
-             yourpropfirm_form_field('yourpropfirm_mt_version', array(
+            woocommerce_form_field('yourpropfirm_mt_version', array(
                 'type' => 'select',
                 'class' => array('form-row-wide ypf_mt_version_field'),
                 'label' => __('MetaTrader Version', 'yourpropfirm'),
@@ -71,9 +71,9 @@ function yourpropfirm_display_custom_field_after_billing_form() {
         <?php
     }
 }
-add_action(' yourpropfirm_after_checkout_billing_form', 'yourpropfirm_display_custom_field_after_billing_form');
+add_action('woocommerce_after_checkout_billing_form', 'yourpropfirm_display_custom_field_after_billing_form');
 
-add_action(' yourpropfirm_checkout_process', 'yourpropfirm_validate_mt_version_field');
+add_action('woocommerce_checkout_process', 'yourpropfirm_validate_mt_version_field');
 function yourpropfirm_validate_mt_version_field() {
     if (isset($_POST['yourpropfirm_mt_version']) && empty($_POST['yourpropfirm_mt_version'])) {
         wc_add_notice(__('Please select a MetaTrader version.', 'yourpropfirm'), 'error');
