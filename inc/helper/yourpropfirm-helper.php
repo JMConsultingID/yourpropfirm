df72d64bd9e0d644674537952bd93add1eabc071
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-24 18:11:32 +0800

    Update yourpropfirm-helper.php

--- inc/helper/yourpropfirm-helper.php
+++ inc/helper/yourpropfirm-helper.php
@@ -8,6 +8,34 @@
  * @package yourpropfirm
  */
 
+function yourpropfirm_send_wp_remote_post_request($endpoint_url, $api_key, $api_data) {
+    $api_url = $endpoint_url;
+    $headers = array(
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'X-Client-Key' => $api_key
+    );
+
+    $response = wp_remote_post(
+        $api_url,
+        array(
+            'timeout' => 30,
+            'redirection' => 5,
+            'headers' => $headers,            
+            'body' => json_encode($api_data)
+        )
+    );
+
+    $http_status = wp_remote_retrieve_response_code($response);
+    $api_response = wp_remote_retrieve_body($response);
+
+    return array(
+        'http_status' => $http_status,
+        'api_response' => $api_response
+    );
+}
+
+
 function yourpropfirm_connection_response_logger() {
     $logger = wc_get_logger();
     $context = array('source' => 'yourpropfirm_connection_response_log');
