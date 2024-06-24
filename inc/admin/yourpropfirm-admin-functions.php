c672cf7b58ecc6ad015c150dec82c28cab2360f7
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-24 18:08:28 +0800

    update core

--- inc/admin/yourpropfirm-admin-functions.php
+++ inc/admin/yourpropfirm-admin-functions.php
@@ -63,13 +63,3 @@ function yourpropfirm_settings_page() {
 function yourpropfirm_addons_page() {
     echo '<div class="wrap"><h1>Addons</h1><p>This section allows you to manage addons for YourPropFirm plugin.</p></div>';
 }
-
-// Hook for adding admin scripts
-add_action('admin_enqueue_scripts', 'yourpropfirm_enqueue_admin_assets');
-// Function to enqueue admin scripts and styles
-function yourpropfirm_enqueue_admin_assets() {
-    // Enqueue CSS file
-    wp_enqueue_style('yourpropfirm-admin-css', plugin_dir_url(__FILE__) . '../../assets/css/yourpropfirm-admin.css');
-    // Enqueue JS file
-    wp_enqueue_script('yourpropfirm-admin-js', plugin_dir_url(__FILE__) . '../../assets/js/yourpropfirm-admin.js', array('jquery'), null, true);
-}
--- inc/helper/yourpropfirm-helper.php
+++ inc/helper/yourpropfirm-helper.php
@@ -13,3 +13,13 @@ function yourpropfirm_connection_response_logger() {
     $context = array('source' => 'yourpropfirm_connection_response_log');
     return array('logger' => $logger, 'context' => $context);
 }
+
+// Hook for adding admin scripts
+add_action('admin_enqueue_scripts', 'yourpropfirm_enqueue_admin_assets');
+// Function to enqueue admin scripts and styles
+function yourpropfirm_enqueue_admin_assets() {
+    // Enqueue CSS file
+    wp_enqueue_style('yourpropfirm-admin-css', plugin_dir_url(__FILE__) . '../../assets/css/yourpropfirm-admin.css');
+    // Enqueue JS file
+    wp_enqueue_script('yourpropfirm-admin-js', plugin_dir_url(__FILE__) . '../../assets/js/yourpropfirm-admin.js', array('jquery'), null, true);
+}
