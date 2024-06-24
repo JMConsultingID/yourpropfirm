3e7214597c76d671f4004a8c7f889215e7b2a3a6
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-24 18:56:33 +0800

    update core

---
+++ inc/public/yourpropfirm-public-functions-checkout.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Plugin functions and definitions for Admin.
+ *
+ * For additional information on potential customization options,
+ * read the developers' documentation:
+ *
+ * @package yourpropfirm
+ */
+// Additional Settings fo Woocommerce
+add_filter('woocommerce_checkout_registration_enabled', '__return_false');
+add_filter('woocommerce_checkout_login_enabled', '__return_false');
+add_filter('option_woocommerce_enable_signup_and_login_from_checkout', '__return_false');
+add_filter('pre_option_woocommerce_enable_signup_and_login_from_checkout', '__return_false');
+
+function yourpropfirm_display_custom_field_after_billing_form() {
+    $plugin_enabled = get_option('yourpropfirm_connection_enabled');
+    $enable_mtversion_field = get_option('yourpropfirm_connection_mt_version_field');
+    $default_mt = get_option('yourpropfirm_connection_default_mt_version_field');
+    $enable_mt_ctrader = get_option('yourpropfirm_connection_enable_mt_ctrader');
+
+    if ($plugin_enabled !== 'enable' || $enable_mtversion_field !== 'enable') {
+        return;
+    }
+
+    $options = ['' => __('Select Meta Trader Version', 'yourpropfirm')]; // Default prompt option
+
+    if ($enable_mt_ctrader === 'enable') {
+        $options['CTrader'] = __('CTrader', 'yourpropfirm');
+    } else {
+        if ($default_mt === 'CTrader') {
+            $options = array(
+                '' => __('Select MetaTrader Version*', 'yourpropfirm'),
+                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
+                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
+            );
+        } elseif ($default_mt === 'MT5') {
+            $options = array(
+                '' => __('Select MetaTrader Version*', 'yourpropfirm'),
+                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
+                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
+            );
+        } elseif ($default_mt === 'MT4') {
+            $options = array(
+                '' => __('Select MetaTrader Version*', 'yourpropfirm'),
+                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
+                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
+            );
+        } else { // Default to MT4 if not MT5 or CTrader
+            $options = array(
+                '' => __('Select MetaTrader Version*', 'yourpropfirm'),         
+                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
+                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
+            );
+        }
+    }
+
+    ?>
+    <div class="yourpropfirm_mt_version yourpropfirm_mt_version_field_wrapper">
+        <?php
+        woocommerce_form_field('yourpropfirm_mt_version', array(
+            'type' => 'select',
+            'class' => array('form-row-wide ypf_mt_version_field'),
+            'label' => __('MetaTrader Version', 'yourpropfirm'),
+            'required' => true,
+            'options' => $options // Use the conditional options here
+        ), '');
+        ?>
+    </div>
+    <?php
+}
+add_action('woocommerce_after_checkout_billing_form', 'yourpropfirm_display_custom_field_after_billing_form');
+
+add_action('woocommerce_checkout_process', 'yourpropfirm_mt_version_validate_field');
+function yourpropfirm_mt_version_validate_field() {
+    if (isset($_POST['yourpropfirm_mt_version']) && empty($_POST['yourpropfirm_mt_version'])) {
+        wc_add_notice(__('Please select a MetaTrader version.', 'yourpropfirm'), 'error');
+    }
+}
--- inc/public/yourpropfirm-public-functions-order.php
+++ inc/public/yourpropfirm-public-functions-order.php
@@ -7,74 +7,12 @@
  *
  * @package yourpropfirm
  */
-// Additional Settings fo Woocommerce
-add_filter('woocommerce_checkout_registration_enabled', '__return_false');
-add_filter('woocommerce_checkout_login_enabled', '__return_false');
-add_filter('option_woocommerce_enable_signup_and_login_from_checkout', '__return_false');
-add_filter('pre_option_woocommerce_enable_signup_and_login_from_checkout', '__return_false');
-
-function yourpropfirm_display_custom_field_after_billing_form() {
-    $plugin_enabled = get_option('yourpropfirm_connection_enabled');
-    $enable_mtversion_field = get_option('yourpropfirm_connection_mt_version_field');
-    $default_mt = get_option('yourpropfirm_connection_default_mt_version_field');
-    $enable_mt_ctrader = get_option('yourpropfirm_connection_enable_mt_ctrader');
-
-    if ($plugin_enabled !== 'enable' || $enable_mtversion_field !== 'enable') {
-        return;
-    }
-
-    $options = ['' => __('Select Meta Trader Version', 'yourpropfirm')]; // Default prompt option
-
-    if ($enable_mt_ctrader === 'enable') {
-        $options['CTrader'] = __('CTrader', 'yourpropfirm');
-    } else {
-        if ($default_mt === 'CTrader') {
-            $options = array(
-                '' => __('Select MetaTrader Version*', 'yourpropfirm'),
-                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
-                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
-            );
-        } elseif ($default_mt === 'MT5') {
-            $options = array(
-                '' => __('Select MetaTrader Version*', 'yourpropfirm'),
-                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
-                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
-            );
-        } elseif ($default_mt === 'MT4') {
-            $options = array(
-                '' => __('Select MetaTrader Version*', 'yourpropfirm'),
-                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
-                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
-            );
-        } else { // Default to MT4 if not MT5 or CTrader
-            $options = array(
-                '' => __('Select MetaTrader Version*', 'yourpropfirm'),         
-                'MT4' => __('MetaTrader Version 4', 'yourpropfirm'),
-                'MT5' => __('MetaTrader Version 5', 'yourpropfirm'),
-            );
-        }
-    }
-
-    ?>
-    <div class="yourpropfirm_mt_version yourpropfirm_mt_version_field_wrapper">
-        <?php
-        woocommerce_form_field('yourpropfirm_mt_version', array(
-            'type' => 'select',
-            'class' => array('form-row-wide ypf_mt_version_field'),
-            'label' => __('MetaTrader Version', 'yourpropfirm'),
-            'required' => true,
-            'options' => $options // Use the conditional options here
-        ), '');
-        ?>
-    </div>
-    <?php
-}
-add_action('woocommerce_after_checkout_billing_form', 'yourpropfirm_display_custom_field_after_billing_form');
-
-add_action('woocommerce_checkout_process', 'yourpropfirm_mt_version_validate_field');
-function yourpropfirm_mt_version_validate_field() {
-    if (isset($_POST['yourpropfirm_mt_version']) && empty($_POST['yourpropfirm_mt_version'])) {
-        wc_add_notice(__('Please select a MetaTrader version.', 'yourpropfirm'), 'error');
+function yourpropfirm_program_id_post_meta_on_order_creation($order_id) {
+    $items = wc_get_order($order_id)->get_items();
+    foreach ($items as $item) {
+        $product_id = $item->get_product_id();
+        $program_id = get_post_meta($product_id, '_yourpropfirm_program_id', true);
+        update_post_meta($order_id, '_yourpropfirm_programID', $program_id);
     }
 }
 
@@ -94,22 +32,27 @@ function yourpropfirm_mt_version_update_post_meta_on_order_creation($order_id) {
     }
     update_post_meta($order_id, '_yourpropfirm_mt_version', $mt_version_value);
 }
-add_action('woocommerce_new_order', 'yourpropfirm_mt_version_update_post_meta_on_order_creation');
 
 function yourpropfirm_update_additional_post_meta_on_order_creation($order_id) {
     $ypf_connection_completed = 0;
     update_post_meta($order_id, '_yourpropfirm_connection_completed', $ypf_connection_completed);
 }
-add_action('woocommerce_new_order', 'yourpropfirm_update_additional_post_meta_on_order_creation');
 
 function yourpropfirm_display_order_meta_after_billing_admin_order($order) {
     $order_id = $order->get_id();
-    $ypf_meta_version = get_post_meta($order_id, '_yourpropfirm_mt_version', true);
-    $ypf_connection_completed = get_post_meta($order_id, '_yourpropfirm_connection_completed', true);
+    $yourpropfirm_programId = get_post_meta($order_id, '_yourpropfirm_programID', true);
+    $yourpropfirm_meta_version = get_post_meta($order_id, '_yourpropfirm_mt_version', true);
+    $yourpropfirm_connection_completed = get_post_meta($order_id, '_yourpropfirm_connection_completed', true);
     echo '<h3>' . __('YourPropFirm Program Details') . '</h3>';
-    echo '<p><strong>' . __('YourPropFirm MetaVersion') . ':</strong> ' . esc_html($ypf_meta_version) . '</p>';
-    echo '<p><strong>' . __('YourPropFirm Completed') . ':</strong> ' . esc_html($ypf_connection_completed) . '</p>';
+    
+    echo '<p><strong>' . __('YourPropFirm programID') . ':</strong> ' . esc_html($yourpropfirm_programId) . '</p>';
+    echo '<p><strong>' . __('YourPropFirm MetaVersion') . ':</strong> ' . esc_html($yourpropfirm_meta_version) . '</p>';
+    echo '<p><strong>' . __('YourPropFirm Completed') . ':</strong> ' . esc_html($yourpropfirm_connection_completed) . '</p>';
     echo '</div>';
 
 }
+
+add_action('woocommerce_new_order', 'yourpropfirm_program_id_post_meta_on_order_creation');
+add_action('woocommerce_new_order', 'yourpropfirm_mt_version_post_meta_on_order_creation');
+add_action('woocommerce_new_order', 'yourpropfirm_additional_post_meta_on_order_creation');
 add_action('woocommerce_admin_order_data_after_billing_address', 'yourpropfirm_display_order_meta_after_billing_admin_order', 10, 1);
