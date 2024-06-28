deef18675c8909249760f5f15a4017c21fc40375
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-28 15:08:32 +0800

    Update yourpropfirm-public-functions-order.php

--- inc/public/yourpropfirm-public-functions-order.php
+++ inc/public/yourpropfirm-public-functions-order.php
@@ -36,7 +36,7 @@ function yourpropfirm_program_id_post_meta_on_order_creation($order_id) {
 function yourpropfirm_display_order_meta_after_billing_admin_order($order) {
     $order_id = $order->get_id();
     $yourpropfirm_program_id = get_post_meta($order_id, '_yourpropfirm_order_program_id', true);
-    $yourpropfirm_meta_version = get_post_meta($order_id, '_yourpropfirm_program_id', true);
+    $yourpropfirm_meta_version = get_post_meta($order_id, '_yourpropfirm_mt_version', true);
     $yourpropfirm_connection_completed = get_post_meta($order_id, '_yourpropfirm_connection_completed', true);
 
     echo '<h3>' . __('YourPropFirm Program Details') . '</h3>';
