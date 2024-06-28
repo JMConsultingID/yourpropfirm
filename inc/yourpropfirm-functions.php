35bc1ec7f159308a443bb4ca05183c248cccd85a
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-28 15:37:19 +0800

    Update yourpropfirm-functions.php

--- inc/yourpropfirm-functions.php
+++ inc/yourpropfirm-functions.php
@@ -15,7 +15,8 @@ require dirname(__FILE__) . '/admin/yourpropfirm-admin-functions-products.php';
 
 // Include public functions
 require dirname(__FILE__) . '/public/yourpropfirm-public-functions.php';
-require dirname(__FILE__) . '/public/yourpropfirm-public-functions-api.php';
+require dirname(__FILE__) . '/public/yourpropfirm-public-functions-api-helper.php';
+require dirname(__FILE__) . '/public/yourpropfirm-public-functions-api-send.php';
 require dirname(__FILE__) . '/public/yourpropfirm-public-functions-checkout.php';
 require dirname(__FILE__) . '/public/yourpropfirm-public-functions-order.php';
 
