4b8af9296f7ef963904d891c818af07c758d8b21
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-09-17 18:02:38 +0800

    Update yourpropfirm-public-functions-api-send.php

--- inc/public/yourpropfirm-public-functions-api-send.php
+++ inc/public/yourpropfirm-public-functions-api-send.php
@@ -58,6 +58,7 @@ function yourpropfirm_send_api_on_order_status_change($order_id, $old_status, $n
 
             $order_currency = $order->get_currency();
             $order_total = $order->get_total();
+            $order_total_value = round((float)$order_total, 2);
             
             // Retrieve the profitSplit, use default if not set or empty
             $profitSplit = $order->get_meta('profitSplit');
