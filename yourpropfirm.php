ff7642da5219947f62d421b44654390cba45742b
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-09-17 14:58:23 +0800

    Update yourpropfirm.php

--- yourpropfirm.php
+++ yourpropfirm.php
@@ -28,18 +28,16 @@ if (!function_exists('is_plugin_active')) {
     include_once(ABSPATH . '/wp-admin/includes/plugin.php');
 }
 
-require plugin_dir_path( __FILE__ ) . 'inc/yourpropfirm-functions.php';
-
 // Enqueue external JS in the WooCommerce product admin page from plugin directory
 function yourpropfirm_enqueue_admin_script($hook) {
     global $post;
 
     // Check if we are on the product edit page
     if ('post.php' === $hook && 'product' === $post->post_type) {
-        // Register and enqueue your external JS from plugin directory
+        // Use plugin_dir_url to ensure correct path
         wp_enqueue_script(
             'yourpropfirm-admin-js', // Unique handle for the script
-            plugin_dir_url(__FILE__) . 'admin/js/yourpropfirm-admin.js', // Path to your JS file in plugin directory
+            plugin_dir_url(__FILE__) . 'assets/js/yourpropfirm-admin.js', // Corrected path to your JS file
             array('jquery'), // Dependencies (in this case, jQuery)
             YOURPROPFIRM_VERSION, // Version of the script
             true // Load in footer
@@ -47,3 +45,6 @@ function yourpropfirm_enqueue_admin_script($hook) {
     }
 }
 add_action('admin_enqueue_scripts', 'yourpropfirm_enqueue_admin_script');
+
+
+require plugin_dir_path( __FILE__ ) . 'inc/yourpropfirm-functions.php';
