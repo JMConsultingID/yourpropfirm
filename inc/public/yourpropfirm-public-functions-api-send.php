43f86a58e42e6d1d4b2a8850fa8d86d01220cae7
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-09-18 12:44:44 +0800

    Update yourpropfirm-public-functions-api-send.php

--- inc/public/yourpropfirm-public-functions-api-send.php
+++ inc/public/yourpropfirm-public-functions-api-send.php
@@ -61,10 +61,18 @@ function yourpropfirm_send_api_on_order_status_change($order_id, $old_status, $n
             $json_encoded_total = json_encode(array('order_total' => $order_total_value));
             $decoded_total = json_decode($json_encoded_total, true);
             $order_total = round((float)$decoded_total['order_total'], 2);
+            $order_total_val = floatval($decoded_total['order_total']);
+            $order_total_val_json = json_encode($order_total);
+            $order_total_val_json_float = json_encode($order_total_val);
             $log_data['logger']->info($order_total_value,  $log_data['context']);
             $log_data['logger']->info($decoded_total,  $log_data['context']);
             $log_data['logger']->info($decoded_total['order_total'],  $log_data['context']);
             $log_data['logger']->info($order_total,  $log_data['context']);
+            $log_data['logger']->info("---------",  $log_data['context']);
+            $log_data['logger']->info($order_total_val,  $log_data['context']);
+            $log_data['logger']->info($order_total_val_json,  $log_data['context']);
+            $log_data['logger']->info($order_total_val_json_float,  $log_data['context']);
+
             
             // Retrieve the profitSplit, use default if not set or empty
             $profitSplit = $order->get_meta('profitSplit');
